53a9bc13ebb8c02e31c7bac3588f384e195347f6
Sapphyruss <lugi.franklin@outlook.fr>
2023-10-14 06:24:06 -0700

    base_model

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Amenity class """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+        """This is amenity.
+        Attrs : 
+            name : The name of the amenity.
+        """
+
+        name = ""
---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""BaseModel Class."""
+import models
+import datetime
+import uuid
+
+
+class BaseModel:
+    """Defines the class BaseModel."""
+
+    def __init__(self, *args, **kwargs):
+        """Ininitialize the new BaseModel.
+        Args:
+            **kwargs (dict) : Key, value of attributes.
+        """
+        tformat = '%Y-%m-%dT%H:%M:%S.%f'
+        for k, v in kwargs.items():
+            if k == "created_at":
+                v = datetime.strptime(value, tformat)
+            if k == "updated_at":
+                v = datetime.strptime(value, tformat)
+            if k != "__class__":
+                setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        clssname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clssname, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with the instant datetime."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary will all keys and values of the instance."""
+        bmdict = self.__dict__.copy()
+        bmdict["created_at"] = self.created_at.isoformat()
+        bmdict["updated_at"] = self.created_at.isoformat()
+        bmdict["__class__"] = self.__class__.__name__
+        return bmdict
