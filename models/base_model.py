bb83901a8ebad31f0037f251af4a33697be800ef
Sapphyruss <lugi.franklin@outlook.fr>
2023-10-15 05:04:53 -0700

    add dict

--- models/base_model.py
+++ models/base_model.py
@@ -11,20 +11,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Ininitialize the new BaseModel.
         Args:
+            *args : wont be used.
             **kwargs (dict) : Key, value of attributes.
         """
-        tformat = '%Y-%m-%dT%H:%M:%S.%f'
-        for k, v in kwargs.items():
-            if k == "created_at":
-                v = datetime.strptime(value, tformat)
-            if k == "updated_at":
-                v = datetime.strptime(value, tformat)
-            if k != "__class__":
-                setattr(self, k, v)
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, tformat)
+                else:
+                    self.__dict__[key] = value
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
