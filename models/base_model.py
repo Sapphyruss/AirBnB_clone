7e21266ebbe2233bf2d74bb295426a5c3cd2ee7a
Sapphyruss <lugi.franklin@outlook.fr>
2023-10-15 05:06:35 -0700

    add dict

--- models/base_model.py
+++ models/base_model.py
@@ -16,8 +16,8 @@ class BaseModel:
         """
         tformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
